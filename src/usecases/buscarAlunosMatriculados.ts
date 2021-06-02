15f098aa386ca2660ec9c8ec1baf70d5f2357a52
itspedrohrs <itspedrohrs@gmail.com>
2021-06-01 22:51:24 -0300

    refactor: adicionado parametro para informar o nome do aluno ao filtro de pesquisa

--- src/usecases/buscarAlunosMatriculados.ts
+++ src/usecases/buscarAlunosMatriculados.ts
@@ -1,17 +1,32 @@
 import { chamarFuncaoElectron } from "../utils/chamarFuncaoElectron";
 import { isElectron } from "../utils/isElectron";
 
-export default async function buscarAlunosMatriculados() {
+export default async function buscarAlunosMatriculados(itemPesquisa = null) {
   if (isElectron()) {
-    return await chamarFuncaoElectron("buscarAlunosMatriculados");
+    return await chamarFuncaoElectron("buscarAlunosMatriculados",itemPesquisa);
   } else {
-    return [
+
+    const alunosMatriculados = [
       {
         id: 1,
         nome: "José Carlos",
         cpf: "00000000001",
         nomeResponsavel: "Manoela",
       },
+      {
+        id: 2,
+        nome: "Xico",
+        cpf: "00000000002",
+        nomeResponsavel: "João",
+      }
     ];
+
+    if(!itemPesquisa){
+      return alunosMatriculados 
+    } 
+    
+    const regex = new RegExp(`${itemPesquisa.trim()}`, "i");
+
+    return alunosMatriculados.filter((aluno) => regex.test(aluno.nome) === true);
   }
 }
